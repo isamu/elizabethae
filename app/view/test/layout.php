dda9416e759e1b54c7d5357f2e6360fa506334d7
isamu arimoto <isamu@to-kyo.to>
2010-10-14 09:29:14 +0900

    design

--- app/view/test/layout.php
+++ app/view/test/layout.php
@@ -6,10 +6,12 @@ Elizabethae Sample Site
 <link rel='stylesheet' type='text/css' href='/css/site.css'> 
 </head>
 <body>
+<div id="content">
 <h2>
 <?= "this is from test layout template" ?>
 </h2>
 <? $this->render_body() ?>
+</div>
 
 <b>this is template</b>
 </body>

f89c8bbe42cd8f6739188922545f28203ef190a9
isamu arimoto <isamu@to-kyo.to>
2010-10-15 10:06:03 +0900

    change design little bit

--- app/view/wiki/index.php
+++ app/view/wiki/index.php
@@ -16,7 +16,7 @@ $(this).submit(function(event){
 <?= $this->controller->data['text_html']; ?>
 </div>
 <form action="/wiki/<?= $this->controller->data['page_name']; ?>" >
-<textarea name=text><?= $this->controller->data['text']; ?></textarea>
+<textarea name=text><?= $this->controller->data['text']; ?></textarea><br/>
 <input type=hidden name="pagename" value="testpage">
 <input type="submit">
 

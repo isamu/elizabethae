7c978a1ef867fe8f44e6495b4d3dbd97898cfa2a
isamu arimoto <isamu@to-kyo.to>
2010-12-17 06:17:02 +0900

    auto load

--- app/htdocs/index.php
+++ app/htdocs/index.php
@@ -1,6 +1,6 @@
 <?PHP
 require_once(realpath(__DIR__."/../config/") . "/config.php");
-require_once ELIZABETHAE_BASE_DIR . "/lib/dispatcher.php";
+require_once ELIZABETHAE_BASE_DIR . "/lib/bootstrap.php";
 
 new \elizabethae\util\dispatcher();
 ?>
